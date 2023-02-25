b90c7bd7a04f5aa24c3f6375660efadec3ced3ce
Tamer <tamerjb.98@gmail.com>
2023-02-25 21:26:42 +0200

    pre-deploy version

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Calculator.css';
 
 const Calculator = (props) => {
@@ -7,17 +7,17 @@ const Calculator = (props) => {
   // const [tip, settip] = useState(10);
   const [formValues, setFormValues] = useState({
     amount: '10',
-    tip: '',
-    customValue: '',
+    tip: '0',
+    customValue: '0',
     numberOfPepole: '1',
   });
-  const [clicked, setClicked] = useState(false);
+  const [activeButton, setActiveButton] = useState();
 
   const handleReset = () => {
     setFormValues({
-      amount: '',
-      tip: '',
-      customValue: '',
+      amount: '10',
+      tip: '0',
+      customValue: '0',
       numberOfPepole: '1',
     });
   };
@@ -25,15 +25,26 @@ const Calculator = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Do something with the input value
-    console.log(formValues.amount);
+    // console.log(formValues.amount);
   };
-  const click = (e) => {
-    setFormValues({ ...formValues, tip: props.tip });
-    handleClick();
-    console.log(formValues);
+  const Click = (e) => {
+    setActiveButton(e);
+    setFormValues({ ...formValues, tip: e });
+    console.log(e);
   };
-  const handleClick = () => {
-    setClicked(!clicked);
+  const handleTip = () => {
+    let tip = (parseInt(formValues.amount) * parseInt(formValues.tip)) / 100;
+    let customTip =
+      (parseInt(formValues.amount) * parseInt(formValues.customValue)) / 100;
+
+    return formValues.customValue == 0 ? tip : customTip;
+  };
+  const handleTotal = () => {
+    let total = parseInt(formValues.amount) + parseInt(handleTip());
+    if (isNaN(total)) {
+      console.log('total', total);
+    }
+    return total / parseInt(formValues.numberOfPepole);
   };
 
   return (
@@ -41,11 +52,12 @@ const Calculator = (props) => {
       <form className='calculator__form' onSubmit={handleSubmit}>
         <div className='calculator__wrapper'>
           <div className='calculator__element calculator__input-container'>
-            <label htmlFor='money-input'>Enter amount:</label>
+            <label htmlFor='money-input'>Bill:</label>
             <input
               type='number'
               step='0.01'
               min='1'
+              max='1000'
               id='money-input'
               className='calculator__input'
               value={formValues.amount}
@@ -54,52 +66,80 @@ const Calculator = (props) => {
               }
               placeholder='$0.00'
             />
+            <span class='icon'></span>
           </div>
-          <label htmlFor='custom--input'>Select Tip:</label>
+          <div>
+            <label htmlFor='custom--input'>Select Tip:</label>
 
-          <div className='grid-container '>
-            <button
-              className={clicked ? 'grid-item grid-item_active' : 'grid-item'}
-              onClick={click}
-            >
-              5%
-            </button>
-            <button
-              className={clicked ? 'grid-item grid-item_active' : 'grid-item'}
-              onClick={click}
-            >
-              10%
-            </button>
-            <button
-              className={clicked ? 'grid-item grid-item_active' : 'grid-item'}
-              onClick={click}
-            >
-              15%
-            </button>
-            <button
-              className={clicked ? 'grid-item grid-item_active' : 'grid-item'}
-              onClick={click}
-            >
-              25%
-            </button>
-            <button
-              className={clicked ? 'grid-item grid-item_active' : 'grid-item'}
-              onClick={click}
-            >
-              50%
-            </button>
-            <input
-              type='number'
-              id='custom-input'
-              className='grid-item-custom'
-              value={formValues.customValue}
-              onChange={(e) =>
-                setFormValues({ ...formValues, customValue: e.target.value })
-              }
-              placeholder='Custom'
-            />
+            <div className='grid-container '>
+              <button
+                className={
+                  activeButton === 5
+                    ? 'grid-item grid-item_active'
+                    : 'grid-item'
+                }
+                onClick={() => Click(5)}
+                value='5'
+              >
+                5%
+              </button>
+              <button
+                className={
+                  activeButton === 10
+                    ? 'grid-item grid-item_active'
+                    : 'grid-item'
+                }
+                onClick={() => Click(10)}
+                value='10'
+              >
+                10%
+              </button>
+              <button
+                className={
+                  activeButton === 15
+                    ? 'grid-item grid-item_active'
+                    : 'grid-item'
+                }
+                onClick={() => Click(15)}
+              >
+                15%
+              </button>
+              <button
+                className={
+                  activeButton === 25
+                    ? 'grid-item grid-item_active'
+                    : 'grid-item'
+                }
+                onClick={() => Click(25)}
+              >
+                25%
+              </button>
+              <button
+                className={
+                  activeButton === 50
+                    ? 'grid-item grid-item_active'
+                    : 'grid-item'
+                }
+                onClick={() => Click(50)}
+              >
+                50%
+              </button>
+              <input
+                type='number'
+                id='custom-input'
+                className={
+                  activeButton === 60 ? 'grid-item-custom' : 'grid-item-custom'
+                }
+                onClick={() => Click(60)}
+                // value={formValues.customValue}
+                onChange={(e) =>
+                  setFormValues({ ...formValues, customValue: e.target.value })
+                }
+                placeholder='Custom'
+              />
+            </div>
           </div>
-          <div className='calculator__element calculator__input-container'>
+          <div className='calculator__element calculator__input-container calculator__input-container-ppl'>
             <label htmlFor='pepole-input'>Number of Pepole:</label>
             <input
               type='number'
@@ -110,30 +150,30 @@ const Calculator = (props) => {
               onChange={(e) =>
                 setFormValues({ ...formValues, numberOfPepole: e.target.value })
               }
-              placeholder='$0.00'
             />
+            <span class='icon'></span>
           </div>
         </div>
 
-        <div className='calculator__wrapper'>
+        <div className='calculator__wrapper style2'>
           <div className='calculator__total'>
-            <p>
-              Tip Amout
-              <span>/Person</span>
-            </p>
-
-            <p>${formValues.amount}</p>
+            <div>
+              <p className='calculator__total-title'>Tip Amout</p>
+              <p className='calculator__total-title-span'>/Person</p>
+            </div>
+            <p className='calculator__total-tip'>${handleTip()}</p>
           </div>
           <div className='calculator__total'>
-            <p>
-              Total
-              <span>/Person</span>
-            </p>
-
-            <p>${formValues.amount}</p>
+            <div>
+              <p className='calculator__total-title'>Total</p>
+              <p className='calculator__total-title-span'>/Person</p>
+            </div>
+            <p className='calculator__total-tip'>${handleTotal()}</p>
           </div>
 
-          <button onClick={handleReset}>Reset</button>
+          <button onClick={handleReset} className='calculator__form-button'>
+            Reset
+          </button>
         </div>
       </form>
     </div>
